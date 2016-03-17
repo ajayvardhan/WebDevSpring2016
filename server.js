37affc2f4dfab694fdfe1ac9af2f41b5bb1e72a8
Ajay Vardhan <ajyvardan@gmail.com>
2016-03-17 16:59:50 -0400

    cookie parser secret

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(multer());
 app.use(session({ secret: process.env.webdev_secret }));
-app.use(express.cookieParser(process.env.webdev_secret));
 app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
