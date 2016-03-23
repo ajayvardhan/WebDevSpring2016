65e746b2e02b7fd01a0bf2b503ea1aadbea1cfb9
Ajay Vardhan <ajyvardan@gmail.com>
2016-03-23 09:16:56 -0400

    server test

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var cookieParser  = require('cookie-parser');
 var session       = require('express-session');
 var mongoose       = require('mongoose');
 
-//var db = mongoose.connect('mongodb://localhost/NowWatching');
+var db = mongoose.connect('mongodb://localhost/NowWatching');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,6 +19,6 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 require("./public/assignment/server/app.js")(app);
-require("./public/project/server/app.js")(app, mongoose);
+require("./public/project/server/app.js")(app, db, mongoose);
 
 app.listen(port, ipaddress);
